61defbc0095f2e0f0aa30f85ba5ab05396a34a80
gorgemul <zeyang793@gmail.com>
2025-06-21 09:25:24 +0800

    _

--- tree/rbt.py
+++ tree/rbt.py
@@ -13,11 +13,6 @@ class RBTNode:
         print(f"{'\033[30m' if self.color == BLACK else '\033[31m'}{4 * level * " "}{prefix}-{self.val}{'\033[0m'}")
         if self.left: self.left.dump(level+1, prefix="l")
         if self.right: self.right.dump(level+1, prefix="r")
-    def search(self, val):
-        if self.val == NIL: return None
-        if val == self.val: return self
-        if val < self.val: return self.left.search(val)
-        if val > self.val: return self.right.search(val)
     def _left_rotation(self, parent, right_child):
         left_child, left_grandson, right_grandson = parent.left, right_child.left, right_child.right
         right_child.val, parent.val = parent.val, right_child.val
@@ -28,14 +23,6 @@ class RBTNode:
         left_child.val, parent.val = parent.val, left_child.val
         parent.left, parent.right, left_grandson.parent = left_grandson, left_child, parent
         left_child.left, left_child.right, right_child.parent = right_grandson, right_child, left_child
-    def _find_pred(self, val, first=True):
-        if first:
-            node = self.search(val)
-            if not node or node.left.val == NIL: return None
-            return node.left._find_pred(val, False)
-        else:
-            if self.val == NIL: return self.parent
-            return self.right._find_pred(val, False)
     def insert(self, val):
         if val == self.val: return # only support distinct value
         if self.val == NIL:
@@ -49,25 +36,6 @@ class RBTNode:
             self.left.insert(val)
         elif val > self.val:
             self.right.insert(val)
-    def delete(self, val):
-        if val < self.val:
-            self.left = self.left.delete(val)
-        elif val > self.val:
-            self.right = self.right.delete(val)
-        else:
-            if self.left.val == NIL and self.right.val == NIL: return RBTNode(NIL, self)
-            if self.left.val != NIL and self.right.val == NIL:
-                self.left.parent = self
-                return self.left
-            if self.left.val == NIL and self.right.val != NIL:
-                self.right.parent = self
-                return self.right
-            pred = self._find_pred(self.val)
-            if pred:
-                self.val, pred.val = pred.val, self.val
-                self.left = self.left.delete(val)
-        return self
-
     def _triangle_case(self, grandparent, parent):
         return (grandparent.left.val == parent.val and parent.right.val == self.val) or (grandparent.right.val == parent.val and parent.left.val == self.val)
     def _fix(self):
@@ -100,7 +68,6 @@ tree.insert(1)
 tree.insert(4)
 tree.insert(14)
 tree.insert(16)
-tree.delete(4)
 tree.dump()
 
 """
